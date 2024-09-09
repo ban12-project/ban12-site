a9a379b6f9c4635098b7880a341e5211783b3ffb
Coda <o4oban@gmail.com>
2024-09-09 22:37:45 +0800

    Add common plugins

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -2,14 +2,19 @@ import type { Config } from 'tailwindcss'
 
 // We want each package to be responsible for its own content.
 const config: Omit<Config, 'content'> = {
+  darkMode: ['class'],
   theme: {
-    extend: {
-      backgroundImage: {
-        'glow-conic':
-          'conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)',
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animate'),
+    require('@ban12/tailwindcss-safe-area'),
+  ],
 }
 export default config
