50fd0767bcc474dd9bb45ab479dbd12ba8a822d3
Coda <o4oban@gmail.com>
2024-09-26 20:06:46 +0800

    Remove sentry because bloats up bundle size with next-on-pages

--- apps/shortcuts/next.config.ts
+++ apps/shortcuts/next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
 import bundleAnalyzer from '@next/bundle-analyzer'
-import { withSentryConfig } from '@sentry/nextjs'
 
 if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform()
@@ -22,37 +21,6 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default withSentryConfig(
-  bundleAnalyzer({
-    enabled: process.env.ANALYZE === 'true',
-  })(nextConfig),
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: 'ban12',
-    project: 'javascript-nextjs',
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Upload a larger set of source maps for prettier stack traces (increases build time)
-    widenClientFileUpload: true,
-
-    // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    // tunnelRoute: "/monitoring",
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-  },
-)
+export default bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig)
