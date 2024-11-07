16e5df554f268c0d187801ec53dcba99a4a7d28f
Coda <o4oban@gmail.com>
2024-11-07 12:35:05 +0800

    Bypass the issue where clicking the cancel button does not trigger

--- apps/shortcuts/components/ui/page-drawer.tsx
+++ apps/shortcuts/components/ui/page-drawer.tsx
@@ -46,10 +46,7 @@ export default function PageDrawer({
       {...rest}
     >
       <Drawer.Portal>
-        <Drawer.Overlay
-          className="fixed inset-0 z-50 bg-black/40"
-          onClick={() => setOpen(false)}
-        />
+        <Drawer.Overlay className="fixed inset-0 z-50 bg-black/40" />
         <Drawer.Content
           className={cn(
             'fixed bottom-0 left-0 right-0 z-50 flex h-[96%] flex-col rounded-t-[10px] bg-white outline-none lg:left-auto lg:h-full lg:w-[460px] lg:rounded-none dark:bg-black',
@@ -67,9 +64,11 @@ export default function PageDrawer({
             className="p-safe-max-4 flex justify-between"
             id={PAGE_DRAWER_HEADER_ID}
           >
-            <Button variant="ios" size="auto" onClick={() => setOpen(false)}>
-              {messages.common.cancel}
-            </Button>
+            <Drawer.Close asChild>
+              <Button variant="ios" size="auto">
+                {messages.common.cancel}
+              </Button>
+            </Drawer.Close>
 
             {header}
           </div>
