a5ce0ce75a31e69806135aa78fd1970ff6d9ea7f
Coda <o4oban@gmail.com>
2024-11-25 18:06:29 +0800

    Support WebAuthn / Passkeys to login dashboard

--- apps/shortcuts/lib/auth.ts
+++ apps/shortcuts/lib/auth.ts
@@ -1,5 +1,8 @@
+import { DrizzleAdapter } from '@auth/drizzle-adapter'
 import NextAuth, { type NextAuthConfig } from 'next-auth'
+import { encode } from 'next-auth/jwt'
 import Credentials from 'next-auth/providers/credentials'
+import Passkey from 'next-auth/providers/passkey'
 import { pathToRegexp } from 'path-to-regexp'
 import { z } from 'zod'
 
@@ -12,24 +15,57 @@ const dashboardRoutes = ['/dashboard'].map(
 )
 const loginRoute = `/(${Object.keys(i18n.locales).join('|')})/login`
 
+const adapter = DrizzleAdapter(db)
+
 const authConfig = {
+  adapter,
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
+    authorized({ auth, request }) {
       const isLoggedIn = !!auth?.user
       const isDashboard = dashboardRoutes.some((path) =>
-        pathToRegexp(path).exec(nextUrl.pathname),
+        pathToRegexp(path).exec(request.nextUrl.pathname),
       )
-      const isOnLogin = pathToRegexp(loginRoute).exec(nextUrl.pathname)
+      const isOnLogin = pathToRegexp(loginRoute).exec(request.nextUrl.pathname)
       if (isDashboard) {
         if (isLoggedIn) return true
         return false // Redirect unauthenticated users to login page
       } else if (isLoggedIn && isOnLogin) {
-        return Response.redirect(new URL('/dashboard', nextUrl))
+        return Response.redirect(new URL('/dashboard', request.nextUrl))
       }
       return true
+    },
+    jwt({ token, user, account }) {
+      if (account?.provider === 'credentials') {
+        token.credentials = true
+      }
+      return token
+    },
+  },
+  jwt: {
+    encode: async function (params) {
+      if (params.token?.credentials) {
+        const sessionToken = crypto.randomUUID()
+
+        if (!params.token.sub) {
+          throw new Error('No user ID found in token')
+        }
+
+        const createdSession = await adapter.createSession?.({
+          sessionToken,
+          userId: params.token.sub,
+          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days
+        })
+
+        if (!createdSession) {
+          throw new Error('Failed to create session')
+        }
+
+        return sessionToken
+      }
+      return encode(params)
     },
   },
   providers: [
@@ -52,7 +88,9 @@ const authConfig = {
         return null
       },
     }),
+    Passkey,
   ],
+  experimental: { enableWebAuthn: true },
 } satisfies NextAuthConfig
 
 export const {
