aad342b2dcfcf60d113fc8b19d71cff9f933c60d
Coda <o4oban@gmail.com>
2025-03-28 12:26:25 +0800

    Update Lenis integration for UI package
    Refactor Lenis scroll implementation with new Tempus dependency and
    improve component structure with proper typings and refs

--- packages/ui/src/lenis.tsx
+++ packages/ui/src/lenis.tsx
@@ -2,29 +2,34 @@
 
 import 'lenis/dist/lenis.css'
 
-import { useEffect, useLayoutEffect } from 'react'
-import Tempus from '@darkroom.engineering/tempus'
-import { useGSAP } from '@gsap/react'
+import { useEffect, useLayoutEffect, useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
-import { ReactLenis, useLenis } from 'lenis/react'
+import { ReactLenis, useLenis, type LenisRef } from 'lenis/react'
+import Tempus from 'tempus'
+import { useTempus } from 'tempus/react'
 
 interface Props extends React.ComponentProps<typeof ReactLenis> {
   children?: React.ReactNode
+  /**
+   * gsap integration
+   */
   gsap?: boolean
+  /**
+   * gsap ScrollTrigger integration
+   */
   scrollTrigger?: boolean
 }
 
 function ScrollTriggerConfig() {
   useLayoutEffect(() => {
-    gsap.registerPlugin(ScrollTrigger, useGSAP as object)
+    gsap.registerPlugin(ScrollTrigger)
     ScrollTrigger.clearScrollMemory('manual')
     ScrollTrigger.defaults({
       markers: process.env.NODE_ENV === 'development',
     })
   }, [])
 
-  // eslint-disable-next-line @typescript-eslint/unbound-method
   const lenis = useLenis(ScrollTrigger.update)
   useEffect(() => ScrollTrigger.refresh(), [lenis])
 
@@ -40,7 +45,7 @@ function GSAPIntergration({ scrollTrigger = false }) {
     gsap.ticker.remove(gsap.updateRoot)
     Tempus?.add((time: number) => {
       gsap.updateRoot(time / 1000)
-    }, 0)
+    })
   }, [])
 
   return scrollTrigger && <ScrollTriggerConfig />
@@ -52,10 +57,20 @@ export default function Lenis({
   scrollTrigger,
   ...props
 }: Props) {
+  const lenisRef = useRef<LenisRef>(null)
+
+  useTempus((time: number) => {
+    if (lenisRef.current?.lenis) {
+      lenisRef.current.lenis.raf(time)
+    }
+  })
+
   return (
     <>
       {gsap && <GSAPIntergration scrollTrigger={scrollTrigger} />}
-      <ReactLenis {...props}>{children}</ReactLenis>
+      <ReactLenis {...props} ref={lenisRef}>
+        {children}
+      </ReactLenis>
     </>
   )
 }
