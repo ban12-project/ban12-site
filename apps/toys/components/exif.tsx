cc69d990ca29d3b4331ce6f3cdcf57ea95719a49
Coda <o4oban@gmail.com>
2025-05-21 15:38:10 +0800

    Styling exif page and supported drag/drop file

--- apps/toys/components/exif.tsx
+++ apps/toys/components/exif.tsx
@@ -1,16 +1,26 @@
 'use client'
 
-import { ChangeEventHandler } from 'react'
+import { ChangeEventHandler, useCallback } from 'react'
 import { Input } from '@repo/ui/components/input'
 import { Label } from '@repo/ui/components/label'
 import { parseMetadata } from '@uswriting/exiftool'
 import { toast } from 'sonner'
+import { useDragDrop } from '#/hooks/use-drag-drop'
 
 export default function Exif() {
+  const callback = useCallback((files: File[]) => {
+    files.forEach(process)
+  }, [])
+  
+  useDragDrop(() => window, callback)
+
   const handleFileChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     const file = event.target.files?.[0]
     if (!file) throw new Error('File is required')
+    process(file)
+  }
 
+  const process = (file: File) => {
     const id = toast.promise(parseMetadata(file), {
       duration: Infinity,
       loading: 'Loading...',
@@ -34,9 +44,11 @@ export default function Exif() {
   }
 
   return (
-    <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="picture">Picture</Label>
-      <Input id="picture" type="file" onChange={handleFileChange} />
+    <div className="flex justify-center items-center min-h-screen">
+      <form className="max-w-sm">
+        <Label htmlFor="file" className='mb-2'>Select a file</Label>
+        <Input id="file" type="file" onChange={handleFileChange} />
+      </form>
     </div>
   )
 }
