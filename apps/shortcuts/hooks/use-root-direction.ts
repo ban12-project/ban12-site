a4c20cbb3e5cb14516f60e10cb748c3c7d0a6c13
Coda <o4oban@gmail.com>
2025-06-03 11:35:31 +0800

    Refactor useRootDirection hook to utilize useIsomorphicLayoutEffect for improved performance

--- apps/shortcuts/hooks/use-root-direction.ts
+++ apps/shortcuts/hooks/use-root-direction.ts
@@ -1,4 +1,5 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import { useIsomorphicLayoutEffect } from 'usehooks-ts'
 
 type Direction = 'ltr' | 'rtl'
 
@@ -7,7 +8,7 @@ export default function useRootDirection<P>(transform: (dir: Direction) => P): P
 export default function useRootDirection<R>(transform?: (dir: Direction) => R) {
   const [direction, setDirection] = useState<Direction>('ltr')
 
-  useEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     const direction = document
       .querySelector('html')!
       .getAttribute('dir') as Direction
