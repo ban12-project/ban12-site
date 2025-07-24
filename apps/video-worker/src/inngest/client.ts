d4c51d7bd17d6d8344e59bdb743272f95272c8a3
Coda <o4oban@gmail.com>
2025-07-24 15:06:01 +0800

    Update Inngest client ID to 'video-worker' for consistency in video worker module

--- apps/video-worker/src/inngest/client.ts
+++ apps/video-worker/src/inngest/client.ts
@@ -1,4 +1,4 @@
 import { Inngest } from 'inngest'
 import { schemas } from "./types";
 
-export const inngest = new Inngest({ id: 'ban12', schemas })
+export const inngest = new Inngest({ id: 'video-worker', schemas })
