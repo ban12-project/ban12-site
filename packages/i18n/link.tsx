b4661040f91f4b6276c3c8e7f63bba9d0ac274a6
Coda <o4oban@gmail.com>
2025-08-02 11:12:51 +0800

    Revert i18n link

--- packages/i18n/link.tsx
+++ packages/i18n/link.tsx
@@ -2,7 +2,6 @@
 
 import { forwardRef } from 'react'
 import NextLink from 'next/link'
-import { usePathname } from 'next/navigation'
 
 import { useLocale } from './i18n'
 
@@ -12,18 +11,14 @@ export const Link = forwardRef<React.ComponentRef<'a'>, Props>(function Link(
   { href, ...rest },
   forwardedRef,
 ) {
-  const { i18n } = useLocale()
-  const pathname = usePathname()
-  const segment = pathname.split('/')[1]
-  const locale = Object.keys(i18n.locales).includes(segment) ? segment : null
+  const { locale } = useLocale()
 
   const isExternal =
     typeof href === 'string'
       ? href.startsWith('http')
       : Boolean(href.pathname?.startsWith('http'))
 
-  if (isExternal || !locale)
-    return <NextLink {...rest} href={href} ref={forwardedRef} />
+  if (isExternal) return <NextLink {...rest} href={href} ref={forwardedRef} />
 
   const hrefWithLocale =
     typeof href === 'string'
