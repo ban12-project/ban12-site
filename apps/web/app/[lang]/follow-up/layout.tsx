1e3cd9e2200b8a42b20e989574ef13d7399e1cf7
Coda <o4oban@gmail.com>
2025-08-04 23:15:45 +0800

    Add loading component and update package manager version

--- apps/web/app/[lang]/follow-up/layout.tsx
+++ apps/web/app/[lang]/follow-up/layout.tsx
@@ -1,13 +1,11 @@
-import { Suspense } from 'react'
-
 export default function Layout(props: {
   children: React.ReactNode
   drawer: React.ReactNode
 }) {
   return (
-    <Suspense>
+    <>
       {props.children}
       {props.drawer}
-    </Suspense>
+    </>
   )
 }
